021d568b60d8dc955c966dfe5bbc1fc78f2d3097
Roxy <necropsia32@gmail.com>
2022-10-26 21:08:13 +0100

    Agregando otro párametro a la función Suma a la rama2

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 /* =========== Roxy Pérez - Día 5 ===========
     ***** Funciones - Reto 2 ***** */
 
-function add(num1, num2) {
+function add(num1, num2, num3) {
     let result;
-    if (isNaN(num1) || (isNaN(num2))) {
+    if (isNaN(num1) || (isNaN(num2)) || (isNaN(num3))) {
         return console.log("Por favor, verifica que ambos sean números");
     }
-    result = num1 + num2;
-    return `Resultado de ${num1} + ${num2} = ` + result.toString();
+    result = num1 + num2 + num3;
+    return `Resultado de ${num1} + ${num2} + ${num3} = ` + result.toString();
 }
 
 function subs(num1, num2) {
--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 let myLib = require("./index");
 
-console.log(myLib.add(56, 4))
+console.log(myLib.add(56, 4, 10))
 console.log(myLib.subs(56, 4))
 console.log(myLib.mult(56, 4))
 console.log(myLib.div(56, 4))
