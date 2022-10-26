ea4439876a65dae55e0cf1c095ae573ead57e047
Roxy <necropsia32@gmail.com>
2022-10-26 20:04:54 +0100

    Quitando la función Suma de la rama1

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 /* =========== Roxy Pérez - Día 5 ===========
     ***** Funciones - Reto 2 ***** */
 
-function add(num1, num2) {
-    let result;
-    if (isNaN(num1) || (isNaN(num2))) {
-        return console.log("Por favor, verifica que ambos sean números");
-    }
-    result = num1 + num2;
-    return `Resultado de ${num1} + ${num2} = ` + result.toString();
-}
-
 function subs(num1, num2) {
     let result;
     if (isNaN(num1) || (isNaN(num2))) {
--- run.js
+++ run.js
@@ -1,6 +1,5 @@
 let myLib = require("./index");
 
-console.log(myLib.add(56, 4))
 console.log(myLib.subs(56, 4))
 console.log(myLib.mult(56, 4))
 console.log(myLib.div(56, 4))
