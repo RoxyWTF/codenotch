65a7d771700e8fca00ae0bcb9ce082ebc1a338a4
RoxyWTF <necropsia32@gmail.com>
2022-10-26 13:46:35 +0100

    Uploading my first GIT challenge for Codenotch

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# codenotch
+# Codenotch - Retos GIT - Día 6 
 Projects for the bootcamp
+
+### by **Roxy**
+1. run.js => llamadas a las funciones declaradas en index.js
---
+++ run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index");
+
+console.log(myLib.add(56, 4))
+console.log(myLib.subs(56, 4))
+console.log(myLib.mult(56, 4))
+console.log(myLib.div(56, 4))
+console.log(myLib.square(12))
